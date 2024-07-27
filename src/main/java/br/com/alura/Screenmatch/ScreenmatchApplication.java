db3f40e70e66ba4b2875c56dfb1e02aff1f228c3
ericmatheus <ericmangabeira@gmail.com>
2024-07-26 22:40:47 -0300

    Sequencia de desenvolvimento do projeto alura

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.Screenmatch;
 
+import br.com.alura.Screenmatch.model.DadosSerie;
 import br.com.alura.Screenmatch.service.ConsumoAPI;
+import br.com.alura.Screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,7 +23,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConsumoAPI consumoAPI = new ConsumoAPI();
 		System.out.print("FILME: ");
 		String apikey = "bc3d9b9c";
-		System.out.println(consumoAPI.obterDados("https://www.omdbapi.com/?t="+getData.nextLine()+"&apikey="+apikey));
-
+		//System.out.println(consumoAPI.obterDados("https://www.omdbapi.com/?t="+getData.nextLine()+"&apikey="+apikey));
+		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=braking+bad&apikey="+apikey);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 }
---
+++ src/main/java/br/com/alura/Screenmatch/service/IConverteDados.java
@@ -0,0 +1,9 @@
+package br.com.alura.Screenmatch.service;
+
+public interface IConverteDados {
+
+    <T> T obterDados(String json, Class<T> classe);
+
+
+
+}
